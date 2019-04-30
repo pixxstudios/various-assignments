e0b114f9dcf714451d3472e79cfdc92625a6b506
Gagandeep Singh <gagan.ddun@gmail.com>
2019-04-30 20:31:47 +1000

    code update

--- longest_substring/substring.js
+++ longest_substring/substring.js
@@ -8,6 +8,17 @@ var getSubstring = function (S1, S2) {
     // check if both strings are same
     if (S1 === S2)
         return S1;
+    //find substring
+    var substr = '';
+    for (var i = 0; i < S1.length; i++) {
+        for (var j = 0; j < S2.length; j++) {
+            if (S1[i] != S2[j])
+                break;
+            substr += S1[i];
+            console.log(S1[i], S2[j], substr);
+        }
+    }
+    return substr;
 };
 exports.getSubstring = getSubstring;
 getSubstring('ABCDE', 'AABCF');
--- longest_substring/substring.ts
+++ longest_substring/substring.ts
@@ -6,6 +6,18 @@ const getSubstring = (S1: string, S2: string): string => {
 
     // check if both strings are same
     if(S1 === S2) return S1;
+
+    //find substring
+    let substr = '';
+    for(let i=0 ; i<S1.length; i++) {
+        for(let j=0; j<S2.length; j++) {
+            if(S1[i] != S2[j]) break;
+            substr += S1[i];
+
+            console.log(S1[i], S2[j], substr);
+        }
+    }
+    return substr;
 }
 
 export { getSubstring };
