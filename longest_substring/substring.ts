fb867b4e00d33813da5489eeec50bb5ca058488e
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-01 18:54:26 +1000

    code changes

--- longest_substring/substring.ts
+++ longest_substring/substring.ts
@@ -8,7 +8,15 @@ const getSubstring = (S1: string, S2: string): string => {
     if(S1 === S2) return S1;
 
     //find substring
-    let substr = '';
+    const len = [...S1].length > [...S2].length ? [...S1].length : [...S2].length;
+    var i = 0;
+    console.log(len);
+    while(i < len && [...S1][i] == [...S2][i]) {
+        i++;
+        console.log([...S1], [...S2]);
+    }
+    console.log(S2.substr(0,i));
+    /*let substr = '';
     for(let i=0 ; i<S1.length; i++) {
         for(let j=0; j<S2.length; j++) {
             if(S1[i] != S2[j]) break;
@@ -16,10 +24,10 @@ const getSubstring = (S1: string, S2: string): string => {
 
             console.log(S1[i], S2[j], substr);
         }
-    }
-    return substr;
+    } */
+    //return substr;
 }
 
 export { getSubstring };
 
-getSubstring('ABCDE', 'AABCF');
+getSubstring('ABCDE', 'AABCFYTR');
