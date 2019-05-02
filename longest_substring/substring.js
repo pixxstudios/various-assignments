5dd575abb7f39bf3af14948cfbf2a0522fcf88fb
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-02 19:56:50 +1000

    removed commented code

--- longest_substring/substring.js
+++ longest_substring/substring.js
@@ -1,5 +1,6 @@
 "use strict";
 // For given two sustrings S1 and S2, find the longest substring common in both the strings
+// check the spec file for tests
 exports.__esModule = true;
 var getSubstring = function (S1, S2) {
     // check for empty values
@@ -9,24 +10,6 @@ var getSubstring = function (S1, S2) {
     if (S1 === S2)
         return S1;
     //find substring
-    var len = S1.slice().length > S2.slice().length ? S1.slice().length : S2.slice().length;
-    var i = 0;
-    console.log(len);
-    while (i < len && S1.slice()[i] == S2.slice()[i]) {
-        i++;
-        console.log(S1.slice(), S2.slice());
-    }
-    console.log(S2.substr(0, i));
-    /*let substr = '';
-    for(let i=0 ; i<S1.length; i++) {
-        for(let j=0; j<S2.length; j++) {
-            if(S1[i] != S2[j]) break;
-            substr += S1[i];
-
-            console.log(S1[i], S2[j], substr);
-        }
-    } */
-    //return substr;
 };
 exports.getSubstring = getSubstring;
 getSubstring('ABCDE', 'AABCFYTR');
--- longest_substring/substring.ts
+++ longest_substring/substring.ts
@@ -1,4 +1,5 @@
 // For given two sustrings S1 and S2, find the longest substring common in both the strings
+// check the spec file for tests
 
 const getSubstring = (S1: string, S2: string): string => {
     // check for empty values
@@ -8,24 +9,7 @@ const getSubstring = (S1: string, S2: string): string => {
     if(S1 === S2) return S1;
 
     //find substring
-    const len = [...S1].length > [...S2].length ? [...S1].length : [...S2].length;
-    var i = 0;
-    console.log(len);
-    while(i < len && [...S1][i] == [...S2][i]) {
-        i++;
-        console.log([...S1], [...S2]);
-    }
-    console.log(S2.substr(0,i));
-    /*let substr = '';
-    for(let i=0 ; i<S1.length; i++) {
-        for(let j=0; j<S2.length; j++) {
-            if(S1[i] != S2[j]) break;
-            substr += S1[i];
-
-            console.log(S1[i], S2[j], substr);
-        }
-    } */
-    //return substr;
+    
 }
 
 export { getSubstring };
